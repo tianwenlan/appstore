174a094b97b0a49eb9d68775a324cfff5cdc6145
Wenlan Tian <tianwenlan@Wenlans-MacBook-Pro.local>
2016-02-05 10:47:59 -0800

    scrapy data from webpage

---
+++ appstore/spiders/huawei.py
@@ -0,0 +1,36 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+import scrapy
+import re
+from scrapy.selector import Selector
+from appstore.items import AppstoreItem
+
+class HuaweiSpider(scrapy.Spider):
+	name = "huawei"
+	allowed_domains = ["huawei.com"]
+
+	start_urls = ["http://appstore.huawei.com/more/all"] 
+
+	def parse(self, response):
+		page = Selector(response)
+
+		divs = page.xpath('//div[@class="game-info  whole"]')
+		
+
+		for div in divs:
+		
+			item = AppstoreItem()
+			item['title'] = div.xpath('//h4[@class="title"]/a/text()').extract_first().encode('utf-8')
+	
+			item['url'] = div.xpath('//h4[@class="title"]/a/@href').extract_first()
+			appid = re.match(r'http://.*/(.*)', item['url']).group(1)
+			item['appid'] = appid
+			item['intro'] = div.xpath('//p[@class="content"]/text()').extract_first().encode('utf-8')
+
+			yield item
+
+
+
