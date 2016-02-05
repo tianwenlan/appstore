8dcd9f7312ca6331f93bc60ff094aa8a242f132f
Wenlan Tian <tianwenlan@Wenlans-MacBook-Pro.local>
2016-02-05 12:30:32 -0800

    fixed write file problem- xpath(.//xxx)

--- appstore/spiders/huawei.py
+++ appstore/spiders/huawei.py
@@ -15,20 +15,17 @@ class HuaweiSpider(scrapy.Spider):
 	start_urls = ["http://appstore.huawei.com/more/all"] 
 
 	def parse(self, response):
+
 		page = Selector(response)
 
 		divs = page.xpath('//div[@class="game-info  whole"]')
-		
 
 		for div in divs:
-		
 			item = AppstoreItem()
-			item['title'] = div.xpath('//h4[@class="title"]/a/text()').extract_first().encode('utf-8')
-	
-			item['url'] = div.xpath('//h4[@class="title"]/a/@href').extract_first()
-			appid = re.match(r'http://.*/(.*)', item['url']).group(1)
-			item['appid'] = appid
-			item['intro'] = div.xpath('//p[@class="content"]/text()').extract_first().encode('utf-8')
+			item['title'] = div.xpath('.//h4[@class="title"]/a/text()').extract_first().encode('utf-8')
+			item['url'] = div.xpath('.//h4[@class="title"]/a/@href').extract_first()
+			item['appid'] = re.match(r'http://.*/(.*)', item['url']).group(1)
+			item['intro'] = div.xpath('.//p[@class="content"]/text()').extract_first().encode('utf-8')
 
 			yield item
 
